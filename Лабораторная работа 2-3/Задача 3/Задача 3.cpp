13a81c1137c7773ab0f2dbed35709a8da289374f
KarpushkinS <c1e2r3g@gmail.com>
2020-09-26 19:50:35 +0300

    Create Задача 3.cpp

---
+++ Лабораторная работа 2-3/Задача 3/Задача 3.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    printf("kx + 2k/x > a\n");
+
+    printf("Введите a: ");
+    float a;
+    scanf_s("%f", &a);
+
+    printf("Введите x: ");
+    float x;
+    scanf_s("%f", &x);
+
+    int k = 0;
+    float y = 0;
+    while (y<a)  // Увеличиваем k пока выражение не станет больше a
+    {
+        k = k + 1;
+        y = k*x + 2*k/x;
+    }
+    printf("k = %d", k);
+}
