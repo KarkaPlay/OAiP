4ccd79e3e41a4697f70a08b33a528e689a275d32
KarpushkinS <c1e2r3g@gmail.com>
2020-09-26 18:46:41 +0300

    Update Задача 2.cpp

--- Лабораторная работа 2-3/Задача 2/Задача 2.cpp
+++ Лабораторная работа 2-3/Задача 2/Задача 2.cpp
@@ -6,30 +6,22 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    printf("ax^2 + bx + c\n");
+    printf("kx + 2k/x > a\n");
 
     printf("Введите a: ");
     float a;
     scanf_s("%f", &a);
 
-    printf("Введите b: ");
-    float b;
-    scanf_s("%f", &b);
+    printf("Введите x: ");
+    float x;
+    scanf_s("%f", &x);
 
-    printf("Введите c: ");
-    float c;
-    scanf_s("%f", &c);
-
-    if (a == 0)  //Если a=0, то решаем как линейное уравнение
+    int k = 0;
+    float y = 0;
+    while (y<a)
     {
-       float x = -c / b;
-       printf("x = %f\n", x);
+        k = k + 1;
+        y = k*x + 2*k/x;
     }
-    else  //Иначе, решаем как квадратное
-    {
-        float d = b*b - 4*a*c;  //Дискриминант
-        float x1 = (-b - sqrt(d))/(2*a);
-        float x2 = (-b + sqrt(d)) / (2 * a);
-        printf("x1 = %f\nx2 = %f", x1, x2);
-    }
+    printf("k = %d", k);
 }
