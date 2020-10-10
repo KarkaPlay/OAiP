0f9dbe71192fac15d360df5d81b9341202b16f21
KarpushkinS <c1e2r3g@gmail.com>
2020-10-10 11:07:52 +0300

    Create Задание 1.cpp

---
+++ Лабораторная работа 1/Задание 1/Задание 1.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>;
+#include <iostream>;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	printf("Выражение 2*x + 3/(x-4)\n");
+	printf("Введите число x\n");
+	float x;
+	scanf_s("%f", &x);
+	if (x == 4)
+		printf("Результат стремится к бесконечности");
+	else {
+		x = 2 * x + 3 / (x - 4);
+		printf("Результат выражения - ");
+		printf("%f", x);
+	}
+}
