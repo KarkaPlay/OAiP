8d2f018fedd58f618166d42a88e109ec61fed316
KarpushkinS <c1e2r3g@gmail.com>
2020-10-10 11:07:56 +0300

    Create Задание 2.cpp

---
+++ Лабораторная работа 1/Задание 2/Задание 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>;
+#include <iostream>;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	printf("Выражение -2 / (x + 5) + 6*x\n");
+	printf("Введите число x\n");
+	int x;
+	scanf_s("%d", &x);
+
+	if (x == -5) {
+		printf("Результат не определен");
+	}
+	else {
+		float y = (float)x;
+		y = -2 / (y + 5) + 6 * y;
+		printf("Результат выражения - ");
+		printf("%f", y);
+	}
+}
