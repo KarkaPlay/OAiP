ccdb59495ea25915e9b96b3e1240fdad397a37e9
KarpushkinS <c1e2r3g@gmail.com>
2020-10-17 16:00:26 +0300

    Create Задание 1.cpp

---
+++ Лабораторная работа 4-5/Задание 1/Задание 1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите размер массива: ";
+    int n;
+    cin >> n;
+    int* N = new int[n];
+
+    cout << "Введите элементы массива: ";
+    for (int i = 0; i < n; i++)
+        cin >> N[i];
+    
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - 1 - i; j++)
+            if (N[j] > N[j + 1])
+            {
+                int buf = N[j];
+                N[j] = N[j + 1];
+                N[j + 1] = buf;
+            }
+
+    for (int i = 0; i < n; i++)
+        cout << N[i] << " ";
+}
