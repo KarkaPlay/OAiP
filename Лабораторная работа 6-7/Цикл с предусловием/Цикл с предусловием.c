1ed54fb59d230806c57fb76438ccc8c81a2e8eb2
KarpushkinS <c1e2r3g@gmail.com>
2020-11-03 23:41:03 +0300

    Update Цикл с предусловием.c

--- Лабораторная работа 6-7/Цикл с предусловием/Цикл с предусловием.c
+++ Лабораторная работа 6-7/Цикл с предусловием/Цикл с предусловием.c
@@ -3,16 +3,16 @@
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    int array[] = { 1, 4, 5, 0, 3, 2 };
+    int array[] = { 1, 4, 5, 0, 3, 2 }; // Объявление массива и необходимых переменных
     int n = sizeof(array) / sizeof(int);
-    float k = 1.247;
+    float k = 1.247; // фактор уменьшения
     int step = n - 1;
-    while (step >= 1) 
+    while (step >= 1) // поак шаг больше 0
     {
-        int i = 0;
+        int i = 0; // начинаем с 0-ого элемента
         while (i < n - step) 
         {
-            if (array[i] > array[i + step]) 
+            if (array[i] > array[i + step]) // если текущий элемент больше, меняем местами
             {
                 int temp = array[i];
                 array[i] = array[i + step];
@@ -20,7 +20,7 @@ int main() {
             }
             i++;
         }
-        step /= k;
+        step /= k; // уменьшаем расстояние на фактор уменьшения
     }
 
 	for (int i = 0; i < n; i++)  // вывод отсортированного массива
