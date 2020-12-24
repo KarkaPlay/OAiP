bd00c8198a3591a0674cfd4d58a1923f089250df
KarpushkinS <c1e2r3g@gmail.com>
2020-12-25 02:25:38 +0300

    Create Цикл с предусловием.c

---
+++ Лабораторная работа 16-17/Цикл с предусловием/Цикл с предусловием.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+void qsort(int* arr, int b, int e) {
+    int piv = arr[(b + e) / 2];
+    int l = b;
+    int r = e;
+    while (l <= r)
+    {
+        while (arr[l] < piv) l++;
+        while (arr[r] > piv) r--;
+        if (l <= r) // условие 
+        {
+            int t = arr[l];
+            arr[l] = arr[r];
+            arr[r] = t;
+            l++;
+            r--;
+        }
+    }
+}
+
+int main()
+{
+    int b, e, l, r, t, arr[] = { 5, 7, 8, 4, 9 };
+    b = 0; e = 3;
+    if (b < r) {
+        qsort(arr, b, r);
+    }
+    if (l < e)
+    {
+        qsort(arr, l, e);
+    }
+
+    for (int i = 0; i < 5; i++)  // Вывод упорядоченного массива
+        printf("%d ", arr[i]);
+}
