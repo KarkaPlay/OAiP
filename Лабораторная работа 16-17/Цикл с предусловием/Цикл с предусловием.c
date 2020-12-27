2481dd88e8cf97f2aab0b60281bba6208573df4a
KarpushkinS <c1e2r3g@gmail.com>
2020-12-27 20:54:25 +0300

    Update Цикл с предусловием.c

--- Лабораторная работа 16-17/Цикл с предусловием/Цикл с предусловием.c
+++ Лабораторная работа 16-17/Цикл с предусловием/Цикл с предусловием.c
@@ -1,36 +1,32 @@
 ﻿#include <stdio.h>
-
 void qsort(int* arr, int b, int e) {
-    int piv = arr[(b + e) / 2];
-    int l = b;
-    int r = e;
-    while (l <= r)
-    {
-        while (arr[l] < piv) l++;
-        while (arr[r] > piv) r--;
-        if (l <= r) // условие 
-        {
-            int t = arr[l];
-            arr[l] = arr[r];
-            arr[r] = t;
-            l++;
-            r--;
+    if (b < e) {
+        int buf, l = b, r = e, piv = arr[(b + e) / 2];  // объявляем необходимые переменные и выбираем опорный элемент
+        while (l <= r) {
+            while (arr[l] < piv) // Движемся от начала массива по возрастающей
+                l++;
+            while (arr[r] > piv) // Движемся от конца массива по убывающей
+                r--;
+            if (l <= r) { // Меняем элементы справа и слева от опорного
+                int t = arr[l];
+                arr[l] = arr[r];
+                arr[r] = t;
+                l++;
+                r--;
+            }
         }
+        qsort(arr, b, r);  // Та же сортировка в правом подмассиве
+        qsort(arr, l, e);  // И в левом
     }
 }
 
 int main()
 {
-    int b, e, l, r, t, arr[] = { 5, 7, 8, 4, 9 };
-    b = 0; e = 3;
-    if (b < r) {
-        qsort(arr, b, r);
-    }
-    if (l < e)
-    {
-        qsort(arr, l, e);
-    }
+    int arr[] = { 5, 7, 8, 4, 9, 1, 3, 6, 2 };  // объявление массива
+    int N = sizeof(arr) / sizeof(int); // длина массива
 
-    for (int i = 0; i < 5; i++)  // Вывод упорядоченного массива
+    qsort(arr, 0, N - 1);  // сортировка
+
+    for (int i = 0; i < N; i++)  // вывод упорядоченного массива
         printf("%d ", arr[i]);
 }
