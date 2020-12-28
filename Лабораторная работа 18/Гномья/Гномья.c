646d29cbe2ae1789fdfcc3aff36440942e1c6f12
KarpushkinS <c1e2r3g@gmail.com>
2020-12-28 11:45:41 +0300

    Create Гномья.c

---
+++ Лабораторная работа 18/Гномья/Гномья.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	int i = 1, j = 2, buf;
+	int arr[100000], n = 100000;
+	unsigned long long int K = 0;
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % n;
+	}
+
+	clock_t begin = clock();
+
+	for (i = 1; i < n;)
+	{
+		if (arr[i - 1] > arr[i]) {
+			buf = arr[i];
+			arr[i] = arr[i - 1];
+			arr[i - 1] = buf;
+			K++;
+			i--;
+			if (i > 0) continue;
+		}
+		i = j++;
+	}
+
+	clock_t end = clock();
+
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+	printf("Time: %f", time_spent);
+	printf("Exchanges: %d ", K);
+}
